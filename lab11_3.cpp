2fa2cf85b16cc4b62a2b5ce418a264e8602d3507
PICHAYOOT HUNCHAINAO <pichayoothunchainao@gmail.com>
2021-12-17 20:09:15 +0700

    เสร็จนสักที

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+
+    int i=0;
+    string text;
+    double sum=0,xbar=0,num;
+
+    while(getline(source,text)){
+        num=atof(text.c_str());
+        sum += num;
+        xbar += pow(num,2);
+        i++;
+    }
+    double mean = sum/i,deviation;
+
+    deviation = sqrt((xbar/i)-pow(mean,2));
+
+    cout << "Number of data = "<< i << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< mean << endl;
+    cout << "Standard deviation = "<< deviation;
+
+    source.close();
+}
